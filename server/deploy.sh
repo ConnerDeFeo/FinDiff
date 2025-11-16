e1ae749b115ae796f6383c121d4d95ed818c87ee
ConnerDeFeo <ninjanerozz@gmail.com>
2025-11-15 19:52:26 -0500

    apparently i was doing a lot of extra work that was not needed

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-cd layers/dynamo/python
+cd layers/utils/python
 pip install -r requirements.txt -t .
 cd ../../filings/python
-pip install -r requirements.txt -t .
-cd ../../utils/python
 pip install -r requirements.txt -t .
 
 cd ../../../../terraform
 terraform apply --auto-approve
 
-cd ../server/layers/dynamo/python
-rm -rf bin boto3** botocore** dateutil jmespath** python** s3transfer** six** urllib3**
+cd ../server/layers/utils
+rm -rf bin certifi** charset** idna** urllib3** requests**
 cd ../../filings/python
 rm -rf bin annotated_types** anyio** beautifulsoup4** boto3** botocore** certifi** bs4** dateutil** h11** httpcore** idna** jmespath** pydantic** s3transfer** six** httpx** python** sniffio** soupsieve** typing_inspection** urllib3** typing_extensions**
-cd ../../utils/python
-rm -rf bin annotated_types** anyio** beautifulsoup4** boto3** botocore** certifi** bs4** dateutil** h11** httpcore** idna** jmespath** pydantic** s3transfer** six** httpx** python** sniffio** soupsieve** typing_inspection** urllib3** typing_extensions** asyncio** charset**
 cd ../../..
